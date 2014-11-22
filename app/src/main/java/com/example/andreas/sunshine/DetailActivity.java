fe9e601c71ed2fb10d6c649474afc2a21d86905b
andihi80 <andreas.vacek@gmail.com>
2014-11-23 00:06:03 +0100

    added a send share action provider to menu

--- app/src/main/java/com/example/andreas/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/andreas/sunshine/DetailActivity.java
@@ -4,15 +4,20 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 
 public class DetailActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -51,24 +56,64 @@ public class DetailActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class DetailFragment extends Fragment {
+    public static class DetailFragment extends Fragment
+    {
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String SHARE_HASHTAG = " # SunshineApp";
+        private String mForecastStr;
+        private ShareActionProvider _actionProvider;
 
-        public DetailFragment() {
+
+        public DetailFragment()
+        {}
+
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            setHasOptionsMenu(true);
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+
+            Intent  intent = getActivity().getIntent();
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView)rootView.findViewById(R.id.textView_fragment_detail)).setText(mForecastStr);
+
+            return rootView;
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+        {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.fragment_detail,menu);
 
-            Intent  intent = getActivity().getIntent();
-            String text = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView)rootView.findViewById(R.id.textView_fragment_detail)).setText(text);
+            MenuItem item = menu.findItem(R.id.action_share);
+            _actionProvider = (ShareActionProvider) item.getActionProvider();
+            setShareIntent(createShareForecastIntent());
+        }
 
-            return rootView;
+        private Intent createShareForecastIntent()
+        {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, mForecastStr + SHARE_HASHTAG);
+            return intent;
+        }
+
+        private void setShareIntent(Intent intent)
+        {
+            if(_actionProvider != null)
+                _actionProvider.setShareIntent(intent);
+            else
+                Log.d(LOG_TAG,"Share action provider is null!");
         }
     }
 }
