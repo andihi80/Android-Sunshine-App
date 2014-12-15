8af787ae39f71a3debadd6537da9eb46c2d9465a
andihi80 <andreas.vacek@gmail.com>
2014-12-15 15:14:23 +0100

    Revert "finished"
    This reverts commit c6f2ad559e9874ad11f4eda6feae497d57fc4934.

--- app/src/main/java/com/example/andreas/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/andreas/sunshine/data/WeatherContract.java
@@ -31,10 +31,14 @@ public class WeatherContract {
     // relationship between a domain name and its website.  A convenient string to use for the
     // content authority is the package name for the app, which is guaranteed to be unique on the
     // device.
+    /* TODO Uncomment for
+    4b - Adding ContentProvider to our Contract
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
     public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
+
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // Possible paths (appended to base content URI for possible URI's)
@@ -42,9 +46,10 @@ public class WeatherContract {
     // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
     // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
     // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+
     public static final String PATH_WEATHER = "weather";
     public static final String PATH_LOCATION = "location";
-
+    */
 
     /* TODO Uncomment for
     4b - Finishing the FetchWeatherTask
@@ -90,16 +95,8 @@ public class WeatherContract {
     }
     */
 
-    public static final class LocationEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-
+    public static final class LocationEntry implements BaseColumns
+    {
         // Table name
         public static final String TABLE_NAME = "location";
 
@@ -115,18 +112,31 @@ public class WeatherContract {
         // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
-    }
-/*
-    /* Inner class that defines the table contents of the weather table */
-    public static final class WeatherEntry implements BaseColumns {
+
+        /* TODO Uncomment for
+        4b - Adding ContentProvider to our Contract
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
 
         public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
 
         public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        */
+
+        /**
+         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * QUIZ - 4b - Adding LocationEntry with ID UriBuilder
+         * https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/e-1604969848/m-1604969849
+         **/
+
+    }
+
+/*
+/* Inner class that defines the table contents of the weather table */
+    public static final class WeatherEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "weather";
 
@@ -157,6 +167,16 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
 
+        /* TODO Uncomment for
+        4b - Adding ContentProvider to our Contract
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static Uri buildWeatherUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
@@ -186,6 +206,6 @@ public class WeatherContract {
 
         public static String getStartDateFromUri(Uri uri) {
             return uri.getQueryParameter(COLUMN_DATETEXT);
-        }
+        }*/
     }
 }
